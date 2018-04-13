43212d3fa331a2a2101d1cd3bfb331be566812a3
Dileep Kumar <ipsdileeprakesh@gmail.com>
2018-04-14 00:34:10 +0530

    New updates

--- Apr-11.sql
+++ Apr-11.sql
@@ -20,7 +20,32 @@ To extract the tar files.
 
 
 2. Deomonstrate the usage of sed command
+ANS:
+SED= STREAM EDITOR
 
+We use sed like, searching, find and replace, insertion or deletion. 
+mostly used for substitution or for find and replace. 
+By using SED you can edit files even without opening it, which is much quicker way to find and replace something in file.
+
+EX:
+cat> sedpractice.txt
+unix is great os. unix is opensource. unix is free os.
+learn operating system.
+unix linux which one you choose.
+unix is easy to learn.unix is a multiuser os.Learn unix .unix is a powerful.
+TO FIND AND REPLACE:
+dileep@ubuntu:~$ sed 's/unix/linux/' sedpractice.txt
+linux is great os. unix is opensource. unix is free os.
+learn operating system.
+linux linux which one you choose.
+linux is easy to learn.unix is a multiuser os.Learn unix .unix is a powerful.
+
+
+Many uses are available in Internet for SED command... Please check it online...
+
+
+
+	
 3. Create citext extension and demonstrate it's usage
 ANS:		CREATE EXTENSION citext;
 EX:	
@@ -44,10 +69,140 @@ select * from mobile where t='my mobile is old mobile';
 (1 row)
 
 4. How to check the table size from OS level
+ANS: 1st we need check table size in Postgres by using meta command (\dt+) 
+then find file path by using select pg_relation_filepath('Table name');
+O/P:  pg_relation_filepath
+----------------------
+ base/12439/352630 
 
-5. Demonstrate the usage of LEFT and RIGHT outer joins
+Then logout from postgres and in O.S. level check the file path to know the Table location.
+
+dileep@ubuntu:cd /opt/postgresql/pg96/data/base/12439 and use grep option to get the table size
+dileep@ubuntu:/opt/postgresql/pg96/data/base/12439$ ls -l | grep 352630
+O/P:
+-rw------- 1 dileep dileep  36249600 Apr  9 05:34 352630
+-rw------- 1 dileep dileep     32768 Apr  9 05:28 352630_fsm
+
+5. Demonstrate the usage of All JOINS, ORDER BY, GROUP BY
+ANS: We need two or more tables for using Joins
+EX:
+select * from client;
+ client_id | client_name
+-----------+-------------
+       100 | Siemens
+       200 | Dell
+       300 | HP
+       400 | Lenovo
+       500 | Nokia
+       600 | Samsung
+      1000 | Apple 
+select * from customers;
+ id |   name   | age |          address          | salary  | client_id
+----+----------+-----+---------------------------+---------+-----------
+  1 | Ramesh   |  32 | Ahmedabad                 | 2000.00 |       100
+  2 | Hardik   |  25 | Delhi                     | 1500.00 |       200
+  3 | kaushik  |  23 | Kota                      | 2000.00 |       300
+  4 | Chaitali |  25 | Mumbai                    | 6500.00 |       400
+  5 | Hardik   |  27 | Bhopal                    | 8500.00 |       500
+  6 | Komal    |  22 | Pune                      | 4500.00 |        60
+  8 | Ramu     |  45 | Bangalore                 | 8000.00 |       800
+
+
+1) JOIN: 
+
+SELECT CUSTOMERS.NAME, client.client_id, client.client_name FROM CUSTOMERS JOIN client ON CUSTOMERS.client_id=client.client_id;
+   name   | client_id | client_name
+----------+-----------+-------------
+ Ramesh   |       100 | Siemens
+ Hardik   |       200 | Dell
+ kaushik  |       300 | HP
+ Chaitali |       400 | Lenovo
+ Hardik   |       500 | Nokia 
+2) LEFT JOIN: 
+
+SELECT CUSTOMERS.NAME, client.client_id, client.client_name FROM CUSTOMERS LEFT JOIN client ON CUSTOMERS.client_id=client.client_id;
+   name   | client_id | client_name
+----------+-----------+-------------
+ Ramesh   |       100 | Siemens
+ Hardik   |       200 | Dell
+ kaushik  |       300 | HP
+ Chaitali |       400 | Lenovo
+ Hardik   |       500 | Nokia
+ Komal    |           |
+ Ramu     |           |
+3) RIGHT JOIN: 
+
+SELECT CUSTOMERS.NAME, client.client_id, client.client_name FROM CUSTOMERS RIGHT JOIN client ON CUSTOMERS.client_id=client.client_id;
+   name   | client_id | client_name
+----------+-----------+-------------
+ Ramesh   |       100 | Siemens
+ Hardik   |       200 | Dell
+ kaushik  |       300 | HP
+ Chaitali |       400 | Lenovo
+ Hardik   |       500 | Nokia
+          |      1000 | Apple
+          |       600 | Samsung
+4) FULL OUTER JOIN: 
+
+SELECT CUSTOMERS.NAME, client.client_id, client.client_name FROM CUSTOMERS FULL OUTER JOIN client ON CUSTOMERS.client_id=client.client_id;
+   name   | client_id | client_name
+----------+-----------+-------------
+ Ramesh   |       100 | Siemens
+ Hardik   |       200 | Dell
+ kaushik  |       300 | HP
+ Chaitali |       400 | Lenovo
+ Hardik   |       500 | Nokia
+ Komal    |           |
+ Ramu     |           |
+          |      1000 | Apple
+          |       600 | Samsung
+
+5) GROUP BY:
+SELECT NAME, sum(SALARY) FROM CUSTOMERS GROUP BY NAME;
+   name   |   sum
+----------+----------
+ Chaitali |  6500.00
+ Ramu     |  8000.00
+ kaushik  |  2000.00
+ Komal    |  4500.00
+ Hardik   | 10000.00
+ Ramesh   |  2000.00
+
+6) ORDER BY:
+SELECT * FROM CUSTOMERS ORDER BY NAME, SALARY;
+ id |   name   | age |          address          | salary  | client_id
+----+----------+-----+---------------------------+---------+-----------
+  4 | Chaitali |  25 | Mumbai                    | 6500.00 |       400
+  2 | Hardik   |  25 | Delhi                     | 1500.00 |       200
+  5 | Hardik   |  27 | Bhopal                    | 8500.00 |       500
+  3 | kaushik  |  23 | Kota                      | 2000.00 |       300
+  6 | Komal    |  22 | Pune                      | 4500.00 |        60
+  1 | Ramesh   |  32 | Ahmedabad                 | 2000.00 |       100
+  8 | Ramu     |  45 | Bangalore                 | 8000.00 |       800
+
+
+
+
+
 
 6. How to find the exact number of rows in a table without using COUNT(*) and reltuples from pg_class
+ANS: 
+TO find ROWS (reltuples)
+select reltuples from pg_class where oid='customers'::regclass;
+ reltuples
+-----------
+         7
+TO find PAGES (relpages)
+select relpages from pg_class where oid='customers'::regclass;
+ relpages
+----------
+        1
+analyze verbose customers;
+INFO:  analyzing "public.customers"
+INFO:  "customers": scanned 1 of 1 pages, containing 7 live rows and 0 dead rows; 7 rows in sample, 7 estimated total rows
+
+
+
 
 7. What is the usage of ANALYZE command
 ANS:
--- Apr-13.sql
+++ Apr-13.sql
@@ -2,10 +2,57 @@
 1. How to drop a parent table without dropping the child table.
 
 2. What is a database system identifier and how to find it.
+ANS: System Identifier number will be assigned when Postgres database is installed in server and The system identifier remains the same even if the server has cloned or backup....etc..
+
+Many actions on the server will keyed to the system identifier.
+
+If anything happens to server, by using system identifier we can find the problems which has happened.
+
+IN OS LEVEL
+
+pg_controldata /opt/postgresql/pg96/data | grep "system identifier";
+Database system identifier:           6519466583466762612
+
 
 3. How to find the query execution duration time from log file.
+ANS: open config file and change log_min_duration_statement value from -1 to 0 and reload the conf file.
+Create a Table with values
+check latest log file from /opt/postgresql/pg96/data/pg_log$ ls -l
+open with cat postgresql-2018-04-13_033808.log
 
-4. What is the difference between TRUNCATE and DELETE statement. (Google)
+4. What is the difference between TRUNCATE, DELETE and DROP statement. (Google)
+
+ 1)TRUNCATE:
+TRUNCATE is a DDL command 
+TRUNCATE is executed using a table lock and whole table is locked for remove all records. 
+We cannot use Where clause with TRUNCATE. 
+TRUNCATE removes all rows from a table. 
+Minimal logging in transaction log, so it is performance wise faster. 
+TRUNCATE TABLE removes the data by deallocating the data pages used to store the table data and records only the page deallocations in the transaction log. 
+Identify column is reset to its seed value if table contains any identity column. 
+To use Truncate on a table you need at least ALTER permission on the table. 
+Truncate uses the less transaction space than Delete statement. 
+Truncate cannot be used with indexed views. 
+ 2)DELETE:
+DELETE is a DML command.
+DELETE is executed using a row lock, each row in the table is locked for deletion.
+We can use where clause with DELETE to filter & delete specific records.
+The DELETE command is used to remove rows from a table based on WHERE condition.
+It maintain the log, so it slower than TRUNCATE.
+The DELETE statement removes rows one at a time and records an entry in the transaction log for each deleted row.
+Identity of column keep DELETE retain the identity.
+To use Delete you need DELETE permission on the table.
+Delete uses the more transaction space than Truncate statement.
+Delete can be used with indexed views.
+ 3)DROP:
+The DROP command removes a table from the database. 
+All the tables' rows, indexes and privileges will also be removed. 
+No DML triggers will be fired. 
+The operation cannot be rolled back.
+DROP and TRUNCATE are DDL commands, whereas DELETE is a DML command.
+DELETE operations can be rolled back (undone), while DROP and TRUNCATE operations cannot be rolled back.
 
 5. Demonstrate the usage of TRANSACTION REPEATABLE READ and SERIALIZATION.
+
+
 
